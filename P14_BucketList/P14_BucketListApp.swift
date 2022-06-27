3c7d38ade6540f61ac3a6fa70340f3d08b860f65
Jae Cho <mobilewiz@gmail.com>
2022-06-27 07:26:17 -0400

    Initial Commit

---
+++ P14_BucketList/P14_BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  P14_BucketListApp.swift
+//  P14_BucketList
+//
+//  Created by Jae Cho on 6/27/22.
+//
+
+import SwiftUI
+
+@main
+struct P14_BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
