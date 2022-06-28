20b234fc6c7ca5ed09dc082b10980a924441b385
Jae Cho <mobilewiz@gmail.com>
2022-06-27 20:40:25 -0400

    Day 68 -2

--- P14_BucketList/ContentView.swift
+++ P14_BucketList/ContentView.swift
@@ -10,8 +10,25 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
-            .padding()
+			 .onTapGesture {
+				 let str = "Test Message"
+				 let url = getDocumentsDirectory().appendingPathComponent("message.txt")
+				 do {
+					 try str.write(to: url, atomically: true, encoding: .utf8)
+					 let input = try String(contentsOf: url)
+					 print(input)
+				 } catch {
+					 print(error.localizedDescription)
+				 }
+			 }
     }
+	
+	func getDocumentsDirectory() -> URL {
+		//find all possible documents directoires for this user
+		let paths = FileManager.default.urls(for: .documentDirectory , in: .userDomainMask)
+		//Just send back the first one, which ought to be the only one
+		return paths[0]
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
