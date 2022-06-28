cc68a636e0eaf130bb4045138f3b79cbcc791c88
Jae Cho <mobilewiz@gmail.com>
2022-06-27 21:05:56 -0400

    Day 69-1 Maps

--- P14_BucketList/ContentView.swift
+++ P14_BucketList/ContentView.swift
@@ -6,21 +6,49 @@
 //
 
 import SwiftUI
+import MapKit
+
+
+struct Location: Identifiable{
+	let id = UUID()
+	let name: String
+	let coordinate: CLLocationCoordinate2D
+}
 
 struct ContentView: View {
+	
+	@State private var mapRegion = MKCoordinateRegion(
+		center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12),
+		span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+	
+	let locations = [
+		Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
+		Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
+	]
+	
+	
     var body: some View {
-        Text("Hello, world!")
-			 .onTapGesture {
-				 let str = "Test Message"
-				 let url = getDocumentsDirectory().appendingPathComponent("message.txt")
-				 do {
-					 try str.write(to: url, atomically: true, encoding: .utf8)
-					 let input = try String(contentsOf: url)
-					 print(input)
-				 } catch {
-					 print(error.localizedDescription)
+		 NavigationView{
+			 Map(coordinateRegion: $mapRegion, annotationItems:  locations){ location in
+				 //MapMarker(coordinate: location.coordinate)
+				 MapAnnotation(coordinate: location.coordinate) {
+	//				 Circle()
+	//					 .stroke(.red, lineWidth: 3)
+	//					 .frame(width: 44, height: 44)
+	//					 .onTapGesture {
+	//						 print("Tapped on \(location.name)")
+	//					 }
+					 NavigationLink{
+						 Text(location.name)
+					 } label: {
+						 Circle()
+						 .stroke(.red, lineWidth: 3)
+						 .frame(width: 44, height: 44)
+					 }
 				 }
 			 }
+			 .navigationTitle("London Explorer")
+		 }
     }
 	
 	func getDocumentsDirectory() -> URL {
